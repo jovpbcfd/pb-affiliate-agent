d841af7902a685e4e5e287a4b74afb4384baba15
jovan_pbcfd@panalobet.com <jovpbcfd>
2025-10-07 18:05:01 +0800

    Update lower content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,16 @@ export default function Home() {
 					className="w-full h-auto"
 				/>
 			</div>
-			<div className="max-w-[400px] mx-auto text-center pt-5 pb-10 md:pb-20 md:pt-10">
-				<h1 className="leading-5 md:leading-[23px] text-[#ffe400] text-md md:text-[1.40rem] font-bold md:mt-20">Message me on 
+			<div className="max-w-[1000px] mx-auto text-center pt-5 pb-10 md:pb-20 md:pt-10">
+				<h1 className="leading-5 md:leading-[28px] text-[#ffe400] text-md md:text-[1.6rem] font-bold md:font-extrabold md:mt-20">Message me on 
 					<br />
 				 Telegram to claim:
 					<br />
-					<span className="text-white font-semibold md:text-[18px]">@henry202577</span>
+					<span className="text-white font-semibold md:text-[20px]">@henry202577</span>
 				</h1>
+				<div className="mt-5 md:mt-10">
+					<p className="text-white md:text-xl">When messaging me, please <span className="text-[#ffe400]">provide a screenshot of your text message.</span></p>
+				</div>
 			</div>
 		</div>
 	);
