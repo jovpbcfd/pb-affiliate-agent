2c0ae455e6757293b83219d7ef7c5d5ae7038cc6
jovan_pbcfd@panalobet.com <jovpbcfd>
2025-10-07 18:16:44 +0800

    Fix lower content  structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ export default function Home() {
 				/>
 			</div>
 			<div className="max-w-[1000px] mx-auto text-center pt-5 pb-10 md:pb-20 md:pt-10">
-				<h1 className="leading-5 md:leading-[28px] text-[#ffe400] text-md md:text-[1.6rem] font-bold md:font-extrabold md:mt-20">Message me on 
+				<div className="mt-4 md:mt-20">
+					<p className="text-white md:text-xl">When messaging me, please <span className="text-[#ffe400]">provide a screenshot of your text message.</span></p>
+				</div>
+				<h1 className="mt-5 leading-5 md:leading-[28px] text-[#ffe400] text-md md:text-[1.6rem] font-bold md:font-extrabold md:mt-10">Message me on 
 					<br />
 				 Telegram to claim:
 					<br />
 					<span className="text-white font-semibold md:text-[20px]">@henry202577</span>
 				</h1>
-				<div className="mt-5 md:mt-10">
-					<p className="text-white md:text-xl">When messaging me, please <span className="text-[#ffe400]">provide a screenshot of your text message.</span></p>
-				</div>
 			</div>
 		</div>
 	);
